b18d7dd55c3e9c473a5800b37afdeef38be67abd
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-07-18 22:32:48 +0300

    Change to noble cause component

--- src/app/pages/causes/create-cause/create-cause.component.ts
+++ src/app/pages/causes/create-cause/create-cause.component.ts
@@ -18,12 +18,12 @@ export class CreateCauseComponent implements OnInit {
   ngOnInit() {}
 
   onFileSelected(event) {
-    console.log('ok');
-    console.log(event);
   }
 
   preview(files) {
-    if (files.length === 0) {return; }
+    if (files.length === 0) {
+      return;
+    }
 
     const mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
@@ -34,6 +34,7 @@ export class CreateCauseComponent implements OnInit {
     const reader = new FileReader();
     this.imagePath = files;
     reader.readAsDataURL(files[0]);
+
     reader.onload = _event => {
       this.imgURL = reader.result;
       this.add();
