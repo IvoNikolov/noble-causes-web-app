7d47269997419257cae0bafeef98276b80a11df1
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-09-05 17:07:37 +0300

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CausesComponent } from './pages/causes/causes.component';
 
 const routes: Routes = [
   {path: '',  component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: }
+  {path: 'causes', component: CausesComponent}
 ];
 
 @NgModule({
